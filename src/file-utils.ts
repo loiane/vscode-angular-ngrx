3e7d977f197dbb2d98da5c28993b09a87e33ab1f
loiane <loianeg@gmail.com>
2017-07-02 13:20:50 -0300

    refactoring: extracted file creation to FileUtils

---
+++ src/file-utils.ts
@@ -0,0 +1,53 @@
+import { IFiles } from './models/file';
+import { IPath } from './models/path';
+import { window } from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+export class FileUtils {
+
+	public createFolder(loc: IPath): Promise<IPath> {
+		return new Promise<IPath>((resolve, reject) => {
+			if (loc.dirName) {
+				fs.exists(loc.dirPath, exists => {
+					if (!exists) {
+						fs.mkdirSync(loc.dirPath);
+						resolve(loc);
+					} else {
+						reject('Folder already exists');
+					}
+				});
+			} else {
+				resolve(loc);
+			}
+		});
+	}
+
+    private writeFiles(files: IFiles[]): Promise<string[]> {
+		return new Promise<string[]>((resolve, reject) => {
+			var errors: string[] = [];
+			files.forEach(file => {
+				fs.writeFile(file.name, file.content, err => {
+					if (err) {
+						errors.push(err.message);
+					}
+					resolve(errors);
+				});
+			});
+		});
+	}
+
+	public createFiles(loc: IPath, files: IFiles[]): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			this.writeFiles(files).then(errors => {
+				if (errors.length > 0) {
+					window.showErrorMessage(
+						`${errors.length} file(s) could not be created.`
+					);
+				} else {
+					resolve(loc.dirPath);
+				}
+			});
+		});
+	}
+}
--- src/ngrx-cli.ts
+++ src/ngrx-cli.ts
@@ -1,12 +1,14 @@
 import { IFiles } from './models/file';
 import { IPath } from './models/path';
 import { FileContents } from './file-contents';
+import { FileUtils } from './file-utils';
 import { window, workspace, TextEditor } from 'vscode';
 import * as path from 'path';
 import * as fs from 'fs';
 
 export class NgrxCli {
 	private fc = new FileContents();
+	private fileUtils = new FileUtils();
 
 	public showFileNameDialog(args, type, defaultTypeName): Promise<IPath> {
 		return new Promise((resolve, reject) => {
@@ -76,54 +78,6 @@ export class NgrxCli {
 		});
 	}
 
-	// Create the new folder
-	public createFolder(loc: IPath): Promise<IPath> {
-		return new Promise<IPath>((resolve, reject) => {
-			if (loc.dirName) {
-				fs.exists(loc.dirPath, exists => {
-					if (!exists) {
-						fs.mkdirSync(loc.dirPath);
-						resolve(loc);
-					} else {
-						reject('Folder already exists');
-					}
-				});
-			} else {
-				resolve(loc);
-			}
-		});
-	}
-
-	public writeFiles(files: IFiles[]): Promise<string[]> {
-		return new Promise<string[]>((resolve, reject) => {
-			var errors: string[] = [];
-			files.forEach(file => {
-				fs.writeFile(file.name, file.content, err => {
-					if (err) {
-						errors.push(err.message);
-					}
-					resolve(errors);
-				});
-			});
-		});
-	}
-
-	// Get file contents and create the new files in the folder
-	public createFiles(loc: IPath, files: IFiles[]): Promise<string> {
-		return new Promise<string>((resolve, reject) => {
-			// write files
-			this.writeFiles(files).then(errors => {
-				if (errors.length > 0) {
-					window.showErrorMessage(
-						`${errors.length} file(s) could not be created. I'm sorry :-(`
-					);
-				} else {
-					resolve(loc.dirPath);
-				}
-			});
-		});
-	}
-
 	public generateUtil = async (loc?: IPath) => {
 		loc.fileName = 'util';
 		loc.dirName = 'store';
@@ -136,8 +90,8 @@ export class NgrxCli {
 			}
 		];
 
-		await this.createFolder(loc);
-		await this.createFiles(loc, files);
+		await this.fileUtils.createFolder(loc);
+		await this.fileUtils.createFiles(loc, files);
 	};
 
 	public generateFeatureStore = async (loc?: IPath) => {
@@ -167,7 +121,7 @@ export class NgrxCli {
 			}
 		];
 
-		await this.createFolder(loc);
-		await this.createFiles(loc, files);
+		await this.fileUtils.createFolder(loc);
+		await this.fileUtils.createFiles(loc, files);
 	};
 }
